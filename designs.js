50269d08f3a27869dd0f180329052b5218cc2b66
sdeutchman <60845532+sdeutchman@users.noreply.github.com>
2020-08-20 03:52:33 +0900

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,27 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
+function makeGrid(rows, columns) {
+	event.preventDefault();
+	const table = document.getElementById("pixelCanvas");
+	var rows = document.getElementById("inputHeight").value;
+	var cells = document.getElementById("inputWidth").value;
+	table.innerHTML = "";
+	for (let i = 0; i < rows; i++) {
+		var row = document.createElement('tr');
+		for (let j = 0; j < cells; j++) {
+			var column = document.createElement('td');
+			row.appendChild(column);
+		}
+		table.appendChild(row);
+	}
+	table.addEventListener('click', function(event) {
+		event.target.style.backgroundColor = document.getElementById('colorPicker').value;
+	});
 
 }
+// should find the submission button
+function formSubmission() {
+	var sizePicker = document.getElementById('sizePicker');
+	document.getElementById('sizePicker').addEventListener('submit', function (event) {
+		makeGrid();
+		sizePicker.submit(function (event) {
+		})
+	});
